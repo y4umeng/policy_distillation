bf5e50a65b91befb5e0720af8ff3318a9f01293f
yw3809 <yw3809@columbia.edu>
2025-03-03 22:44:18 -0500

    reversed setup changes

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+import os.path
+import sys
 import setuptools
 
-DISTNAME = "src"  # Consider using a better package name than "src"
-DESCRIPTION = "A Deep Learning Toolkit for Policy Distillation."
-AUTHOR = "y4umeng"
 
-setuptools.setup(
-    name=DISTNAME,
-    packages=setuptools.find_packages(where="src"),  # Look for packages in src/
-    package_dir={"": "src"},  # Tell setuptools that packages are under src/
-    version="0.1",
-    description=DESCRIPTION,
-    author=AUTHOR,
-)
+if __name__ == "__main__":
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+    DISTNAME = "src"
+    DESCRIPTION = "A Deep Learning Toolkit for Policy Distillation."
+    AUTHOR = "y4umeng"
+    DOCLINES = __doc__
+
+    setuptools.setup(
+        name=DISTNAME,
+        packages=setuptools.find_packages(),
+        version="0.1",
+        description=DESCRIPTION,
+        long_description=DOCLINES,
+        long_description_content_type="text/markdown",
+        author=AUTHOR,
+    )
