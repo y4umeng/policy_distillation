4c0ef0f67cad12ce4ab3f336c6633eb5514d679d
yw3809 <yw3809@columbia.edu>
2025-03-07 17:09:51 -0500

    visualization notebook

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -20,14 +20,15 @@ checkpoint_dicts = {
     "BreakoutNoFrameskip-v4": breakout_checkpoint_dict
 }
 
-def get_model(model_name, env_name, num_actions):
+def get_model(model_name, env_name, num_actions, distiller=""):
     model = model_dict[model_name]
     model = model(num_actions=num_actions)
     if env_name in checkpoint_dicts and model_name in checkpoint_dicts[env_name]:
         path = checkpoint_dicts[env_name][model_name]
         return model, path
     else:
-        path = os.path.join(model_dir, env_name, "dqn")
+        path = os.path.join(model_dir, env_name, model_name)
+        if distiller: path = path + "_" + distiller
         if os.path.exists(path):
             return model, path
         return model, None
