aa81ad33fd5133ac6793deb15e2fae92307f5a5c
yw3809 <yw3809@columbia.edu>
2025-03-13 13:02:41 -0400

    baselines

---
+++ src/baselines.sh
@@ -0,0 +1,58 @@
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn5.yaml
+
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn5.yaml
+
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn5.yaml
+
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn5.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn5.yaml
+
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn6.yaml
+
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn6.yaml
+
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn6.yaml
+
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn6.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn6.yaml
+
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4.yaml
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4.yaml
+
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn4.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn4.yaml
+
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn4.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn4.yaml
+
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn4.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn4.yaml
+
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn3.yaml
+
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn3.yaml
+
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn3.yaml
+
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn3.yaml
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn3.yaml
