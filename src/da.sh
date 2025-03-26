52d51d3747639ec70cd9ea0bc80e5d6a643e2df5
root <yw3809@columbia.edu>
2025-03-26 02:40:30 +0000

    new configs

--- src/da.sh
+++ src/da.sh
@@ -34,6 +34,8 @@ python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn6_da.yaml
 # python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn6_da.yaml
 # python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn6_da.yaml
 
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4_da_00001.yaml
+
 python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4_da.yaml
 # python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4_da.yaml
 
@@ -45,6 +47,8 @@ python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn4_da.yaml
 
 python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn4_da.yaml
 # python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn4_da.yaml
+
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn3_da_00001.yaml
 
 python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn3_da.yaml
 # python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn3_da.yaml
---
+++ src/dqn4_da.sh
@@ -0,0 +1,9 @@
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4_da_00001.yaml
+
+python tools/train.py --cfg configs/MsPacmanNoFrameskip-v4/dqn4_da.yaml
+
+python tools/train.py --cfg configs/SpaceInvadersNoFrameskip-v4/dqn4_da.yaml
+
+python tools/train.py --cfg configs/BeamRiderNoFrameskip-v4/dqn4_da.yaml
+
+python tools/train.py --cfg configs/BreakoutNoFrameskip-v4/dqn4_da.yaml
